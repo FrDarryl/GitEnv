adb3694951654a8e533374f8cdf4fc5527e43f04
Darryl Jordan <revdjordan@gmail.com>
2016-05-03 14:53:19 +0100

    First commit

---
+++ bin/GitEnvInit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd /home/${USER}
+for gitEnvDotfile in ${PWD}/GitEnv/dotfiles/.*;
+do
+    echo "Found ${gitEnvDotfile}"
+    dotfileBasename="$(basename ${gitEnvDotfile})"
+    if [ ${dotfileBasename} = "." ] || [ ${dotfileBasename} = ".." ]; then
+       continue
+    fi
+    if [ -L ${dotfileBasename} ]; then
+        echo "..Deleting existing symlink ${PWD}/${dotfileBasename}"
+        rm ${dotfileBasename}
+    elif [ -f ${dotfileBasename} ]; then
+        echo "..Renaming existing ${PWD}/${dotfileBasename} to ${PWD}/${dotfileBasename}.factory"
+        mv ${dotfileBasename} ${dotfileBasename}.factory
+    fi
+    echo "..Creating ${PWD}/${dotfileBasename} (symbolic link to ${gitEnvDotfile})"
+    ln -s ${gitEnvDotfile} ./${dotfileBasename}
+done
---
+++ dotfiles/.bashrc
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -o noclobber
+set -o vi
+#set -x
+
+fileArray=(~/.bashrc.envars ~/.bashrc.commands)
+for file in ${fileArray[@]}
+do
+    if [ -f ${file} ]
+    then
+        . ${file}
+    fi
+done
+
+[ `type -t setPrompt` == 'function' ] && setPrompt
+
+[ -f ~/perl5/perlbrew/etc/bashrc ] && source ~/perl5/perlbrew/etc/bashrc
+
+#http://serverfault.com/questions/485487/use-bashrc-without-breaking-sftp
+if [[ $- == *i* ]]; then
+#   To see fortunes when this runs, install fortune packages and do a 'touch ~/.fortune'
+    [ -f ~/.fortune ] && [ -f /usr/games/fortune ] && /usr/games/fortune -s
+fi
